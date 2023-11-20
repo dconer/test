7837821e67d7f1821a08d5f253bc721b33ae4da6
data_eng <a20185061@pucp.edu.pe>
2023-11-20 18:27:01 -0500

    add changes

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-91b^gg7c(9e*&kssw9+uqvd5u^=*7mt*7rgl#_@==2(ugm1imm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'upload'
 
