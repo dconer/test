fd91f8742f0c65edf2a7aa2971602d73b70cb56c
data_eng <a20185061@pucp.edu.pe>
2023-11-20 18:50:38 -0500

    configure collect static

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -120,12 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS=[
-        os.path.join(BASE_DIR,'static')
-    ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'upload'
 
